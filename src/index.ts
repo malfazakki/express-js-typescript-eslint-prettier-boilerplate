857f983721ae5a6756a97fe9af435262af6b1b52
Al Fatih Muzakki <malfazakki@gmail.com>
2025-08-19 22:28:57 +0700

    feat(env): add dotenv for environment variable management and update README
    - include dotenv as a dependency in package.json and package-lock.json
    - modify src/index.ts to load environment variables using dotenv
    - add README.md with project setup instructions and features overview

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
-const port: number = 3000;
+const port = process.env.PORT || 3000;
+dotenv.config();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
